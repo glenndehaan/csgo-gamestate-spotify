2ad48b8095c205a4ac1ba4036b10ca10addd020f
Glenn de Haan <glenn@dehaan.cloud>
2018-07-05 21:05:03 +0200

    Added the CSGO logic

--- src/frontend/components/CSGO.js
+++ src/frontend/components/CSGO.js
@@ -7,12 +7,12 @@ export default class extends Component {
         this.state = {
             data: false
         };
+
         this.server = false;
+        this.spotifyReady = false;
+        this.isPlaying = false;
     }
 
-    /**
-     *
-     */
     componentDidMount() {
         this.server = window.httpServer.createServer((req, res) => {
             res.writeHead(200, {'Content-Type': 'text/html'});
@@ -22,6 +22,7 @@ export default class extends Component {
                     this.setState({
                         data: JSON.parse(data.toString())
                     });
+                    this.generalProcessData(JSON.parse(data.toString()));
                 } catch (e) {
                     console.warn(`[WEBDATA] Error retrieving data from API: ${e}`)
                 }
@@ -33,6 +34,38 @@ export default class extends Component {
         });
 
         this.server.listen("3001", "0.0.0.0");
+
+        window.spotifyHelper.player.on('ready', () => {
+            this.spotifyReady = true;
+        });
+    }
+
+    generalProcessData(data) {
+        if (!data.previously && !data.player) return;
+
+        if ((data.player && data.player.activity === 'menu') || (data.player.state.health === 0 || data.player.steamid !== data.provider.steamid) || (data.round && data.round.phase !== "live")) {
+            // Let's play some music
+            if (this.spotifyReady) {
+                if (!this.isPlaying) {
+                    console.log(`[CS::GO] Let's start some music`);
+                    window.spotifyHelper.player.play();
+                    this.isPlaying = true;
+                }
+            } else {
+                console.warn(`[SPOTIFY] Isn't ready to handle requests`);
+            }
+        } else {
+            // Let's be serious so quit the music
+            if (this.spotifyReady) {
+                if (this.isPlaying) {
+                    console.log(`[CS::GO] Stop the music`);
+                    window.spotifyHelper.player.pause();
+                    this.isPlaying = false;
+                }
+            } else {
+                console.warn(`[SPOTIFY] Isn't ready to handle requests`);
+            }
+        }
     }
 
     render() {
